2f508465042f72d9fb08b436cc00fad96011e03a
keita shinohara <keita.shinohara@gmail.com>
2018-11-18 21:25:18 +0900

    apply config file

--- application/flask_blog/__init__.py
+++ application/flask_blog/__init__.py
@@ -1,6 +1,7 @@
 # flask_blog/__init__.py
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)   # アプリケーション本体を作成
+app.config.from_object('flask_blog.config') # config ファイルの設定を config として扱う
 
-import flask_blog.views
+import flask_blog.views # view ファイルをインポートする
