3da1e2bd8fb0bc5dfcc8016bfdd8f3c79bb3cbcd
keita shinohara <keita.shinohara@gmail.com>
2018-11-18 21:26:06 +0900

    apply templete

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -1,6 +1,8 @@
 # flask_blog\views.py
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
+# http://127.0.0.1:5000/ にリクエストがあったときの処理
 @app.route('/')
 def show_entires():
-    return "Hello World!"
+    return render_template('entries/index.html')
